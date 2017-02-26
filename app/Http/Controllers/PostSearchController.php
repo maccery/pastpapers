c6bfe85153868ba56fcc3afb4562bbbffb7fc5e8
Tom Macmichael <tommacmichael@me.com>
2017-02-26 21:30:01 +0000

    Search is more broad now

--- app/Http/Controllers/PostSearchController.php
+++ app/Http/Controllers/PostSearchController.php
@@ -11,11 +11,15 @@ class PostSearchController extends Controller
     public function search(PostSearchRequest $request)
     {
         $search_query = $request->input('query');
+
         $versions = Version::where('version', 'like', '%' . $search_query . '%')
             ->orWhereHas('software', function($q) use ($search_query) {
                 $q->where('name', 'like', '%' . $search_query . '%');
+                foreach (explode(' ', $search_query) as $item)
+                {
+                    $q->orWhere('name', 'like', '%' . $item . '%');
+                }
             })
-            ->orderBy('created_at', 'desc')
             ->get();
 
         return response()->view('search.results', ['versions' => $versions]);
--- resources/views/search/tips.blade.php
+++ resources/views/search/tips.blade.php
@@ -1,6 +1,5 @@
 <p>
 <ul class="list">
-    <li>Try to be less specific, e.g search 'Android' rather than 'Android KitKat'</li>
     <li>You can <a href="{{ route('browse') }}">browse</a> by software name instead</li>
 </ul>
 <p><b>Still can't find what you're looking for?</b>
